20191ed1a0d62b1dcdf1586494f050e51af14d1f
Prakash Shahi <learn@prakashshahi@gmail.com>
2023-04-28 22:31:35 +0545

    initial upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const faviouriteIcons = document.querySelectorAll(".favourite-icon");
+
+// Toggle favourite icon on click
+faviouriteIcons.forEach(icon => {
+    icon.addEventListener("click", (e) => {
+        if(icon.src.includes("images/favourite-icon-2.png")) {
+            icon.src = "images/favourite-icon-active.png";
+        } else {
+            icon.src = "images/favourite-icon-2.png";
+        }
+    });
+});
