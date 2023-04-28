ad98936ef9edce7ba4f89a8075cb75c22641662a
Prakash Shahi <learn@prakashshahi@gmail.com>
2023-04-28 22:41:42 +0545

    some changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const faviouriteIcons = document.querySelectorAll(".favourite-icon");
 // Toggle favourite icon on click
 faviouriteIcons.forEach(icon => {
     icon.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
         if(icon.src.includes("images/favourite-icon-2.png")) {
             icon.src = "images/favourite-icon-active.png";
         } else {
